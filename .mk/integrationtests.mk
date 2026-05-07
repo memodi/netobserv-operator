ce396d2870ec511d4b19c22e8f988f7f9918d54f
memodi <memodi@redhat.com>
2026-05-07 14:48:43 -0400

    update make targets

--- .mk/integrationtests.mk
+++ .mk/integrationtests.mk
@@ -1,5 +1,9 @@
 ##@ OCP Integration tests
 
-.PHONY: prereqs-kind
+.PHONY: build-integration-tests
 build-integration-tests: ## Build integration tests suite binary with OTE
-	CGO_ENABLED=0 go build -mod=mod -o bin/netobserv-tests-ext ./cmd/netobserv-tests-ext
+	CGO_ENABLED=0 go build -mod=vendor -o bin/netobserv-tests-ext ./cmd/netobserv-tests-ext
+
+.PHONY: image-tests-ext
+image-tests-ext: ## Build tests extension container image
+	podman build -f Dockerfile.tests-ext -t $(REPO)/netobserv-operator-tests-ext:$(VERSION) .
